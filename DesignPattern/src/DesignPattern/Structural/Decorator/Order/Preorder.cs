3bbe8cb952903e7d18405c97a5dce2e02ddbe7a9
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-07 17:45:11 -0300

    [ADD] added Preorder class

---
+++ DesignPattern/src/DesignPattern/Structural/Decorator/Order/Preorder.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern.Structural.Decorator.Order.Repositories;
+using System;
+using System.Linq;
+
+namespace DesignPattern.Structural.Decorator.Order
+{
+    public class Preorder : OrderBase
+    {
+        public Preorder(IProductRepository repository)
+        : base(repository)
+        {
+
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {          
+            return _repository
+                .GetAll()
+                .ToList()
+                .Sum(x => x.Price) * 0.9;
+        }
+    }
+}
