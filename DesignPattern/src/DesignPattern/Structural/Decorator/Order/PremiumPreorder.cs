868fc3dc840f1e0d77a6343f5ad26bb950a30867
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-07 18:01:59 -0300

    [ADD] Added PremiumPreorder class [ALT] Usage class

---
+++ DesignPattern/src/DesignPattern/Structural/Decorator/Order/PremiumPreorder.cs
@@ -0,0 +1,20 @@
+﻿using DesignPattern.Structural.Decorator.Order.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structural.Decorator.Order
+{
+    public class PremiumPreorder : OrderDecorator
+    {
+        public PremiumPreorder(OrderBase order , IProductRepository repository)
+            : base(order, repository)
+        {
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {
+            return base.CalculateTotalOrderPrice() * 0.9;
+        }
+    }
+}
--- DesignPattern/src/DesignPattern/Structural/Decorator/Order/Usage.cs
+++ DesignPattern/src/DesignPattern/Structural/Decorator/Order/Usage.cs
@@ -1,10 +1,20 @@
-﻿namespace DesignPattern.Structural.Decorator.Order
+﻿using System.Diagnostics;
+
+namespace DesignPattern.Structural.Decorator.Order
 {
     public class Usage
     {
         static void Main()
         {
-          
+            var repository = new ProductRepository();
+            var regularOrder = new RegularOrder(repository);
+            Debug.WriteLine(regularOrder.CalculateTotalOrderPrice());            
+
+            var preOrder = new Preorder(repository);
+            Debug.WriteLine(preOrder.CalculateTotalOrderPrice());          
+
+            var premiumPreorder = new PremiumPreorder(preOrder, repository);
+            Debug.WriteLine(premiumPreorder.CalculateTotalOrderPrice());
         }
     }
 }
