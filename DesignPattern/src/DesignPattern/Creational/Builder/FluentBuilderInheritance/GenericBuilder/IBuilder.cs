a7a39e84e8af1208ff467e09a7cc346b355ed34c
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:29:22 -0300

    [ADD] Added IBuilder class

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/FluentBuilderInheritance/GenericBuilder/IBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Creational.Builder.FluentBuilderInheritance.GenericBuilder
+{
+    public interface IBuilder<TEntity>
+        
+        where TEntity : class, new()
+    {
+        TEntity Entity { get; set; }
+        TEntity Build();
+    }
+}
