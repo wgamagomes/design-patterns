61834b98b1b75d69efb6fe520538cd01d327d656
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:12:10 -0300

    [ADD] Added Person class

---
+++ DesignPattern/src/DesignPattern/Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPattern.Entities
+{
+    public class Person : Entity
+    {
+        public string Name { get; set; }
+
+        public DateTime Born { get; set; }
+    }
+}
