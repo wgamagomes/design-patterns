6a68f3471254e36c243500084d0955b197e99a2f
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:39:04 -0300

    [ADD] Added GoldenStudentBuilder

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/FluentBuilderInheritance/Person/GoldStudentBuilder.cs
@@ -0,0 +1,16 @@
+﻿using DesignPattern.Entities;
+
+namespace DesignPattern.Creational.Builder.FluentBuilderInheritance
+{
+    public class GoldStudentBuilder<TBuilder, TEntity> : StudentBuilder<TBuilder, TEntity>
+       where TBuilder : GoldStudentBuilder<TBuilder, TEntity>, new()
+       where TEntity : GoldStudent, new()
+    {
+
+        public TBuilder WithMedia(double media)
+        {
+            Entity.Media = media;
+            return (TBuilder)this;
+        }
+    }
+}
