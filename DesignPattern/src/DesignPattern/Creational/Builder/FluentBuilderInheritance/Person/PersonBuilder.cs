1a67064478fb70bd7a8d72ad46f3e0c0b416a225
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:33:14 -0300

    [ADD] Added PersonBuilder class

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/FluentBuilderInheritance/Person/PersonBuilder.cs
@@ -0,0 +1,25 @@
+﻿using DesignPattern.Creational.Builder.FluentBuilderInheritance.GenericBuilder;
+using DesignPattern.Entities;
+using System;
+
+namespace DesignPattern.Creational.Builder.FluentBuilderInheritance
+{
+    public class PersonBuilder<TBuilder, TEntity> : Builder<TBuilder, TEntity>
+        where TBuilder : PersonBuilder<TBuilder, TEntity>, new()
+        where TEntity : Person, new()
+    {
+
+        public TBuilder WithName(string name)
+        {
+            Entity.Name = name;
+            return (TBuilder)this;
+        }
+
+        public TBuilder WithBorn(DateTime born)
+        {
+            Entity.Born = born;
+            return (TBuilder)this;
+        }
+
+    }    
+}
