081c9ee57fb9aaf0f14811cc7dd57ec687b10fce
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:24:17 -0300

    [ALT] Changed product class

--- DesignPattern/src/DesignPattern/Entities/Product.cs
+++ DesignPattern/src/DesignPattern/Entities/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace DesignPattern.Entities
 {
-    public class Product
+    public class Product : Entity
     {
         public string Name { get; internal set; }
         public int Price { get; internal set; }
--- DesignPattern/src/DesignPattern/Repositories/ProductRepository.cs
+++ DesignPattern/src/DesignPattern/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPattern.Entities;
 using DesignPattern.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace DesignPattern.Repositories
@@ -8,9 +9,9 @@ namespace DesignPattern.Repositories
     {
         public IEnumerable<Product> GetAll()
         {
-            yield return new Product { Name = "Phone", Price = 587 };
-            yield return new Product { Name = "Tablet", Price = 800 };
-            yield return new Product { Name = "PC", Price = 1200 };
+            yield return new Product { Name = "Phone", Price = 587 , Id = Guid.NewGuid()};
+            yield return new Product { Name = "Tablet", Price = 800, Id = Guid.NewGuid() };
+            yield return new Product { Name = "PC", Price = 1200, Id = Guid.NewGuid() };
         }
     }
 }
