0b2eec3e93866e97b28b5a2e6562f308a864a1fd
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:55:02 -0300

    [ADD] Added Usage Class

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/FluentBuilderInheritance/Person/Usage.cs
@@ -0,0 +1,46 @@
+﻿using DesignPattern.Entities;
+using System;
+
+namespace DesignPattern.Creational.Builder.FluentBuilderInheritance
+{
+    public class Usage
+    {
+        static void Main()
+        {
+            Student studentSubClass = StudentBuilderDirector
+                .Create()
+                .WithHobby("hobby")
+                .WithName("Peter")
+                .WithBorn(DateTime.Now)
+                .WithDegree("undergrad")
+                .Build();
+
+            GoldStudent goldStudentSubClass = GoldStudentBuilderDirector
+                .Create()
+                .WithHobby("hobby")
+                .WithName("Peter")
+                .WithBorn(DateTime.Now)
+                .WithDegree("undergrad")
+                .WithMedia(10.0f)
+                .Build();
+
+            Person studentSuperClass = StudentBuilderDirector
+                .Create()
+                .WithHobby("hobby")
+                .WithName("Peter")
+                .WithBorn(DateTime.Now)
+                .WithDegree("undergrad")
+                .Build();
+
+            Person goldStudentSuperClass = GoldStudentBuilderDirector
+                .Create()
+                .WithHobby("hobby")
+                .WithName("Peter")
+                .WithBorn(DateTime.Now)
+                .WithDegree("undergrad")
+                .WithMedia(10.0f)
+                .Build();
+
+        }
+    }
+}
