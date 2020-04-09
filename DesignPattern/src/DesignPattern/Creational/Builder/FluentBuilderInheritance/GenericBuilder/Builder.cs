b2da910aaad587f006d0cf4f10537fe6c73673f4
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-09 08:27:31 -0300

    [ADD] Added Build class

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/FluentBuilderInheritance/GenericBuilder/Builder.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPattern.Creational.Builder.FluentBuilderInheritance.GenericBuilder
+{
+    public class Builder<TBuilder, TEntity> : IBuilder<TEntity>
+        where TBuilder : class, IBuilder<TEntity>, new()
+        where TEntity : class, new()
+    {
+        public Builder()
+        {
+            Entity = new TEntity();
+        }
+        public TEntity Entity { get; set; }
+
+        public TEntity Build() => Entity;
+
+        public static TBuilder Create()
+        {
+            return new TBuilder();
+        }
+    }
+}
