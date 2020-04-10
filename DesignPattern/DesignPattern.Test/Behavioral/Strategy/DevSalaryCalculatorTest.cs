79ceb4a7de7caab599f1dbcc873ade18ebc79705
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-10 16:00:05 -0300

    [ADD] Added SeniorSalaryCalculatorTest method

--- DesignPattern/DesignPattern.Test/Behavioral/Strategy/DevSalaryCalculatorTest.cs
+++ DesignPattern/DesignPattern.Test/Behavioral/Strategy/DevSalaryCalculatorTest.cs
@@ -1,7 +1,32 @@
-﻿namespace DesignPattern.Test.Behavioral.Strategy
+﻿using DesignPattern.Behavioral.Strategy;
+using DesignPattern.Entities;
+using DesignPattern.Enums;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DesignPattern.Test.Behavioral.Strategy
 {
     public class DevSalaryCalculatorTest
     {
 
+
+        [Fact]
+        public void SeniorSalaryCalculatorTest()
+        {
+            IEnumerable<DeveloperReport> reports = new List<DeveloperReport>
+            {
+                new DeveloperReport { Name = "Dev1", Level = DeveloperLevel.Senior, HourlyRate = 30.5, WorkingHours = 160 },
+                new DeveloperReport { Name = "Dev2", Level = DeveloperLevel.Junior, HourlyRate = 20, WorkingHours = 120 },
+                new DeveloperReport { Name = "Dev3", Level = DeveloperLevel.Senior, HourlyRate = 32.5, WorkingHours = 130 },
+                new DeveloperReport { Name = "Dev4", Level = DeveloperLevel.Junior, HourlyRate = 24.5, WorkingHours = 140 }
+            };
+
+
+            var calculatorContext = new SalaryCalculator(new SeniorDevSalaryCalculator());
+
+            var seniorTotal = calculatorContext.Calculate(reports);
+
+            Assert.True(seniorTotal == 10926);
+        }
     }
 }
