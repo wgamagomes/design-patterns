d69383728ae7aedb3ca5a1ae5bff3748cd72fad6
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-10 15:23:14 -0300

    [ADD] Added JuniorDevSalaryCalculator class

---
+++ DesignPattern/src/DesignPattern/Behavioral/Strategy/JuniorDevSalaryCalculator.cs
@@ -0,0 +1,16 @@
+﻿using DesignPattern.Entities;
+using DesignPattern.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPattern.Behavioral.Strategy
+{
+    public class JuniorDevSalaryCalculator : ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports) =>
+            reports
+                .Where(r => r.Level == DeveloperLevel.Junior)
+                .Select(r => r.CalculateSalary())
+                .Sum();
+    }
+}
