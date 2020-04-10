38b6b47c404ed7ae8dff495f25b720d2ea802ac2
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-10 15:15:16 -0300

    [ADD] Added DeveloperReport class

---
+++ DesignPattern/src/DesignPattern/Entities/DeveloperReport.cs
@@ -0,0 +1,12 @@
+﻿using DesignPattern.Enums;
+
+namespace DesignPattern.Entities
+{
+    public class DeveloperReport : Person
+    {
+        public DeveloperLevel Level { get; set; }
+        public int WorkingHours { get; set; }
+        public double HourlyRate { get; set; }
+        public double CalculateSalary() => WorkingHours * HourlyRate;
+    }
+}
