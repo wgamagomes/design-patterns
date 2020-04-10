77993c7b14168f5ac3c1d642927975935248200d
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-10 15:30:45 -0300

    [ADD] Added SalaryCalculator class

---
+++ DesignPattern/src/DesignPattern/Behavioral/Strategy/SalaryCalculator.cs
@@ -0,0 +1,21 @@
+﻿using DesignPattern.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral.Strategy
+{
+    public class SalaryCalculator
+    {
+        private ISalaryCalculator _calculator;
+
+        public SalaryCalculator(ISalaryCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public void SetCalculator(ISalaryCalculator calculator) => _calculator = calculator;
+
+        public double Calculate(IEnumerable<DeveloperReport> reports) => _calculator.CalculateTotalSalary(reports);
+    }
+}
