af00a4b7adde1ca4ed6467a7aeb62274e60830d7
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-10 15:27:58 -0300

    [ADD] Added  SeniorDevSalaryCalculator class

---
+++ DesignPattern/src/DesignPattern/Behavioral/Strategy/SeniorDevSalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using DesignPattern.Entities;
+using DesignPattern.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern.Behavioral.Strategy
+{
+    public class SeniorDevSalaryCalculator : ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports) =>
+            reports
+                .Where(r => r.Level == DeveloperLevel.Senior)
+                .Select(r => r.CalculateSalary() * 1.2)
+                .Sum();
+    }
+}
