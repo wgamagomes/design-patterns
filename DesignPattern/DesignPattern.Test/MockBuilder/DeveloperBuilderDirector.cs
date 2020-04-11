d3202a6d4b0aa8facac6caf7f5ce01ab7891706b
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-11 10:56:21 -0300

    [ADD] Added DeveloperBuilderDirector class

---
+++ DesignPattern/DesignPattern.Test/MockBuilder/DeveloperBuilderDirector.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Entities;
+
+namespace DesignPattern.Test.MockBuilder
+{
+    public class DeveloperBuilderDirector : DeveloperBuilder<DeveloperBuilderDirector, Developer>
+    {
+
+    }
+}
