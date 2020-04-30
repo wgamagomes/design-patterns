89643c621a887467b79cc601c12ccdda9735d376
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-29 23:14:04 -0300

    [ADD] Added Vehicle class

---
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPattern.Creational.AbstractFactory
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+    }
+}
