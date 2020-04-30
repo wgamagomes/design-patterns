27fdb3009d627013350bb957953b0a8e9a383aaf
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-29 23:15:19 -0300

    [ADD] Added TowingServiceFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/TowingServiceFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPattern.Creational.AbstractFactory
+{
+    //Abstract Factory
+    public abstract class TowingServiceFactory
+    {
+        public abstract TowTruck GetTowTruck();
+        public abstract Vehicle GetVehicle(string brand, string model, VehicleSizeClass vehicleSize);
+    }
+}
