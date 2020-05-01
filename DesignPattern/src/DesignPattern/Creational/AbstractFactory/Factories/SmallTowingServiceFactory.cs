9feffc1fd562aa59bab2760c1e89f9b995085d71
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-01 10:53:05 -0300

    [ADD] Added SmallTowingServiceFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/Factories/SmallTowingServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern.Creational.AbstractFactory.Factories
+{
+    public class SmallTowingServiceFactory : TowingServiceFactory
+    {
+        public override TowTruck GetTowTruck()
+        {
+            return TowTruckFactory.GetInstance(Size.Small);
+        }
+        public override Vehicle GetVehicle(string brand, string model)
+        {
+            return VehicleFactory.GetInstance(Size.Small, brand, model);
+        }
+    }
+}
