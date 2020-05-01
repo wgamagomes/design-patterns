07fb94cd6de5a99c3fde9c41d6cf210ff8dd02a3
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-01 10:50:43 -0300

    [ADD] Added MidTowingServiceFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/Factories/LargeTowingServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern.Creational.AbstractFactory.Factories
+{
+    public class LargeTowingServiceFactory : TowingServiceFactory
+    {
+        public override TowTruck GetTowTruck()
+        {
+            return TowTruckFactory.GetInstance(Size.Large);
+        }
+        public override Vehicle GetVehicle(string brand, string model)
+        {
+            return VehicleFactory.GetInstance(Size.Large, brand, model);
+        }
+    }
+}
