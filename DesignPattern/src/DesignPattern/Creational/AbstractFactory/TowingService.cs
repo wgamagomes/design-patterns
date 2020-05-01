90425b1501abcbf81afcfb64347efff7f84103b0
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-01 10:37:34 -0300

    [ADD] Added TowingService class

---
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/TowingService.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPattern.Creational.AbstractFactory
+{
+       
+    public class TowingService
+    {
+        private readonly Vehicle _vehicle;
+        private readonly TowTruck _towTruck;
+
+        public TowingService(TowingServiceFactory towingServiceFactory, string brand, string model)
+        {
+            _vehicle = towingServiceFactory.GetVehicle(brand, model);
+            _towTruck = towingServiceFactory.GetTowTruck();
+        }
+
+
+        public void Tow()
+        {
+            _towTruck.Tow(_vehicle);
+        }
+
+        public static TowingService Create(Size size, string brand, string model)
+        {
+            return new TowingService(TowingServiceFactory.Create(size), brand, model);
+        }
+    }
+}
--- DesignPattern/src/DesignPattern/Creational/AbstractFactory/TowingServiceFactory.cs
+++ DesignPattern/src/DesignPattern/Creational/AbstractFactory/TowingServiceFactory.cs
@@ -4,6 +4,61 @@
     public abstract class TowingServiceFactory
     {
         public abstract TowTruck GetTowTruck();
-        public abstract Vehicle GetVehicle(string brand, string model, VehicleSizeClass vehicleSize);
+        public abstract Vehicle GetVehicle(string brand, string model);
+
+
+        public static TowingServiceFactory Create(Size size)
+        {
+            switch (size)
+            {
+                case Size.Small:
+                    return new SmallTowingServiceFactory();
+
+                case Size.Mid:
+                    return new MidTowingServiceFactory();
+
+                case Size.Large:
+                    return new LargeTowingServiceFactory();
+            }
+
+            return null;
+        }
     }
+
+    public class SmallTowingServiceFactory : TowingServiceFactory
+    {
+        public override TowTruck GetTowTruck()
+        {
+            return TowTruckFactory.GetInstance(Size.Small);
+        }
+        public override Vehicle GetVehicle(string brand, string model)
+        {
+            return VehicleFactory.GetInstance(Size.Small, brand, model);
+        }
+    }
+
+    public class MidTowingServiceFactory : TowingServiceFactory
+    {
+        public override TowTruck GetTowTruck()
+        {
+            return TowTruckFactory.GetInstance(Size.Mid);
+        }
+        public override Vehicle GetVehicle(string brand, string model)
+        {
+            return VehicleFactory.GetInstance(Size.Mid, brand, model);
+        }
+    }
+
+    public class LargeTowingServiceFactory : TowingServiceFactory
+    {
+        public override TowTruck GetTowTruck()
+        {
+            return TowTruckFactory.GetInstance(Size.Large);
+        }
+        public override Vehicle GetVehicle(string brand, string model)
+        {
+            return VehicleFactory.GetInstance(Size.Large, brand, model);
+        }
+    }
+
 }
