51769638654737ae459f7f3196431a1cb616c5a7
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-02 18:05:39 -0300

    [ADD] Added OracleConnection class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Connections/OracleConnection.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    public class OracleConnection : IDbConnection
+    {
+        private string _connectionString;
+
+        public OracleConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Close()
+        {           
+            IsOpen = false;
+        }
+
+        public bool IsOpen { get; private set; }
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+    }
+}
+}
