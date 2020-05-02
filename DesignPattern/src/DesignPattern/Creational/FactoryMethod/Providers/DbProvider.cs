e6ad29569471cce647bc789c914811615162b8fa
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-02 18:06:40 -0300

    [ADD] Added DbProvider class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Providers/DbProvider.cs
@@ -0,0 +1,19 @@
+﻿
+
+using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    //Abstract Product
+    public abstract class DbProvider
+    {
+        protected readonly string _connectionString;
+
+        public DbProvider( string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public abstract IDbConnection GetConnection();
+    }
+}
