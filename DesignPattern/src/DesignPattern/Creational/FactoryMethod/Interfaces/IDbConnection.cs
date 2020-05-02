925b6e6f3213256b1f718f80b021805db935516f
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-02 18:05:13 -0300

    [ADD] Added IDbConnection interface

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Interfaces/IDbConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational.FactoryMethod.Interfaces
+{
+    public interface IDbConnection
+    {
+
+        void Close();
+
+        void Open();
+
+        bool IsOpen { get;  }
+    }
+}
