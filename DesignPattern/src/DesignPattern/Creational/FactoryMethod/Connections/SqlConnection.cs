c805e055bd18555215652649cfc6dbe8e28a2c8a
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-02 18:05:58 -0300

    [ADD] Added SqlConnection class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Connections/SqlConnection.cs
@@ -0,0 +1,26 @@
+﻿using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    public class SqlConnection : IDbConnection
+    {
+        private readonly string _connectionString;
+
+        public SqlConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+        }
+
+        public bool IsOpen { get; private set; }
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+    }
+}
