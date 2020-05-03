ae2254f59eb2eb18d88947485a42866ae19fb1a8
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-03 11:50:24 -0300

    [ADD] Added SqlProvider class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Providers/SqlProvider.cs
@@ -0,0 +1,18 @@
+﻿using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    public  class SqlProvider: DbProvider
+    {
+       // Concrete Product
+        public SqlProvider( string connectionString)
+            :base(connectionString)
+        {            
+        }
+
+        public  override IDbConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
