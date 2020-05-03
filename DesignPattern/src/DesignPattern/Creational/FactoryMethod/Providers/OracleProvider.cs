0c10e722600374dab759c98981decec8557e6039
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-03 11:50:55 -0300

    [ADD] Added OracleProvider class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Providers/OracleProvider.cs
@@ -0,0 +1,21 @@
+﻿
+
+using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    // Concrete Product
+    public  class OracleProvider : DbProvider
+    {
+       
+        public OracleProvider( string connectionString)
+            :base(connectionString)
+        {            
+        }
+
+        public  override IDbConnection GetConnection()
+        {
+            return new OracleConnection(_connectionString);
+        }
+    }
+}
