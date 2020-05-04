b6242d5b76181728861b4b2a2ae2c24133e4f00b
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-04 08:33:49 -0300

    [ADD] Added Usage class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Usage.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Creational.FactoryMethod.Factories;
+using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod
+{
+    public class Usage
+    {
+        static void Main()
+        {
+            IDbConnection sqlConnection = DbProviderFactory
+                 .Choose(Database.Sql)
+                 .GetProvider("")
+                 .GetConnection();
+
+            sqlConnection.Open();
+            sqlConnection.Close();
+
+            IDbConnection oracleConnection = DbProviderFactory
+                 .Choose(Database.Oracle)
+                 .GetProvider("")
+                 .GetConnection();
+
+            oracleConnection.Open();
+            oracleConnection.Close();
+        }
+    }
+}
