34cdecd474ac97c165903fa6eeef9223e70df806
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-04 08:31:52 -0300

    [ADD] Added DbProviderFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Factories/DbProviderFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPattern.Creational.FactoryMethod.Factories
+{
+    // Abstract creator
+    public abstract class DbProviderFactory
+    {
+        // This is the factory method
+        public abstract DbProvider GetProvider(string connectionString);
+
+        public static DbProviderFactory Choose(Database database)
+        {
+            if (database == Database.Sql)
+                return new SqlProviderFactory();
+
+            return new OracleProviderFactory();
+
+        }
+    }
+}
