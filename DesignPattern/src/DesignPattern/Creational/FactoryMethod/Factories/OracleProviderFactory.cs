3c375541b6df35b27e09bd80a31d17661efc42e4
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-04 08:32:23 -0300

    [ADD] Added OracleProviderFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Factories/OracleProviderFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern.Creational.FactoryMethod.Factories
+{
+    // Concrete Creator
+    public class OracleProviderFactory : DbProviderFactory
+    {
+        // This is the factory method
+        public override DbProvider GetProvider(string connectionString)
+        {
+            return new OracleProvider(connectionString);
+        }
+    }
+}
