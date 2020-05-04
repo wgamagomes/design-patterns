952b688f4165bcaf2cab9f2c343b778fb09b6640
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-04 08:33:25 -0300

    [ADD] Added SqlProviderFactory class

---
+++ DesignPattern/src/DesignPattern/Creational/FactoryMethod/Factories/SqlProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern.Creational.FactoryMethod.Interfaces;
+
+namespace DesignPattern.Creational.FactoryMethod.Factories
+{
+    // Concrete Creator
+    public class SqlProviderFactory : DbProviderFactory
+    {
+        // This is the factory method
+        public override DbProvider GetProvider(string connectionString)
+        {
+            return new SqlProvider(connectionString);
+        }
+    }
+}
