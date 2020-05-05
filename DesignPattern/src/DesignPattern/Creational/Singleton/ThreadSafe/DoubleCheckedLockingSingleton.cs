6682a20a57ff5766bb7b233fa3a0fa6be24baad6
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-05 08:57:17 -0300

    [ADD] Added DoubleCheckedLockingSingleton thread safe class

---
+++ DesignPattern/src/DesignPattern/Creational/Singleton/ThreadSafe/DoubleCheckedLockingSingleton.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPattern.Creational.Singleton.ThreadSafe
+{
+    public class DoubleCheckedLockingSingleton
+    {
+        private static DoubleCheckedLockingSingleton _instance;
+        public static int Counter = 0;
+        private static readonly object Instancelock = new object();
+
+        private DoubleCheckedLockingSingleton()
+        {
+            Counter++;
+        }
+
+        public static DoubleCheckedLockingSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DoubleCheckedLockingSingleton();
+                        }                       
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public string GetDetails(string message)
+        {
+            return $"Counter Value: {Counter} - {message}.";
+        }
+    }
+}
