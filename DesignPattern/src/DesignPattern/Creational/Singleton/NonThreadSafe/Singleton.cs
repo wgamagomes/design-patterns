7d8d073d911d09ca72e0f5c27df007799c5725f4
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-05 08:55:36 -0300

    [ADD] Added Singleton class

---
+++ DesignPattern/src/DesignPattern/Creational/Singleton/NonThreadSafe/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPattern.Creational.Singleton.NonThreadSafe
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public static int Counter = 0;
+
+        private Singleton()
+        {
+            Counter++;
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+
+        public string GetDetails(string message)
+        {
+            return $"Counter Value: {Counter} - {message}.";
+        }      
+    }
+}
