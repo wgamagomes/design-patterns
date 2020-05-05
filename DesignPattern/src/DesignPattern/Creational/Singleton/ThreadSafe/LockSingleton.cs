18218283007f446ad53abc4247190bbd12a002ee
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-05 08:56:43 -0300

    [ADD] Added LockSingleton thread safe class

---
+++ DesignPattern/src/DesignPattern/Creational/Singleton/ThreadSafe/LockSingleton.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPattern.Creational.Singleton.ThreadSafe
+{
+    public class LockSingleton
+    {
+        private static LockSingleton _instance;
+        public static int Counter = 0;
+        private static readonly object Instancelock = new object();
+
+        private LockSingleton()
+        {
+            Counter++;
+        }
+
+        public static LockSingleton Instance
+        {
+            get
+            {
+                lock (Instancelock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LockSingleton();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        public string GetDetails(string message)
+        {
+            return $"Counter Value: {Counter} - {message}.";
+        }
+    }
+}
