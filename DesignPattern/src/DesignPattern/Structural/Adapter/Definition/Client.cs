42080290fc3bed7c16bb894bc69b691a904d0f87
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-06 17:35:54 -0300

    [ADD] Added Client class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Definition/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPattern.Structural.Adapter.Definition
+{
+    public class Client
+    {
+        private Target _target;
+
+        public Client()
+        {
+            _target = new Adapter(); //could this object be injected  here in this constructor
+        }
+
+        public void Request()
+        {
+            _target.DoSomething();
+        }
+    }
+}
