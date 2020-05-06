a68e073253db1ed171e106cbdebbc9a8f96dcb49
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-06 17:35:32 -0300

    [ADD] Added Adapter class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Definition/Adapter.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPattern.Structural.Adapter.Definition
+{
+    public class Adapter : Target
+    {
+        private Adaptee _adaptee;
+
+        public Adapter()
+        {
+            _adaptee = new Adaptee();
+        }
+        public override void DoSomething()
+        {
+            // Possibly do some other work  and then call DoSomethingElse method
+            _adaptee.DoSomethingElse();
+        }
+    }
+}
