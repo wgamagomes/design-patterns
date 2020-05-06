047fb3484b26f9a7f3c479cafbe16b42c9924537
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-06 17:35:09 -0300

    [ADD] Added Adaptee class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Definition/Adaptee.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Structural.Adapter.Definition
+{
+    public class Adaptee
+    {
+        public void DoSomethingElse()
+        {
+            // Do something else here
+        }
+    }
+}
