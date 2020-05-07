d97858980882668965da70cbc377bb5a9f82a6f2
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-07 20:24:22 -0300

    [ADD] Added Logger class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Loggers/Logger.cs
@@ -0,0 +1,19 @@
+﻿using DesignPattern.Structural.Adapter.Log.Interfaces;
+using System;
+
+namespace DesignPattern.Structural.Adapter.Log
+{
+    //Target
+    public class Logger : ILog
+    {
+        public void Log(string message)
+        {
+            
+        }
+
+        public void LogError(Exception exception)
+        {
+            
+        }
+    }
+}
