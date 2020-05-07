85226330d8e4d16f8aff24bff628a8e5823be7c0
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-07 20:22:58 -0300

    [ADD] Added ILog interface

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Interfaces/ILog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPattern.Structural.Adapter.Log.Interfaces
+{
+    public interface ILog
+    {
+        void Log(string message);
+
+        void LogError(Exception exception);
+    }
+}
