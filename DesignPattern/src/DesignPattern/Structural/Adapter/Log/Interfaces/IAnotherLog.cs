18ce0461e33d7ee8614ee43d4bf7b12758e81832
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-07 20:23:38 -0300

    [ADD] Added IAnotherLog interface

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Interfaces/IAnotherLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPattern.Structural.Adapter.Log.Interfaces
+{
+    public interface IAnotherLog
+    {
+        void Info(string message);
+
+        void Error(Exception exception);
+    }
+}
