c7533dd337c5e69c9a24dec4f48987104a06a342
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-07 20:24:50 -0300

    [ADD] Added AnotherLogger class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Loggers/AnotherLogger.cs
@@ -0,0 +1,19 @@
+﻿using DesignPattern.Structural.Adapter.Log.Interfaces;
+using System;
+
+namespace DesignPattern.Structural.Adapter.Log
+{
+    //Adaptee
+    public class AnotherLogger : IAnotherLog
+    {
+        public void Error(Exception exception)
+        {
+           
+        }
+
+        public void Info(string message)
+        {
+            
+        }
+    }
+}
