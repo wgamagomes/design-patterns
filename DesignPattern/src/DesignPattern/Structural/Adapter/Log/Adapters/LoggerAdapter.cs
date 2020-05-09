f0b6e537ca560f7d08a3a1243975e6545d32fd59
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-08 22:09:42 -0300

    [ADD] Added LoggerAdapter class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Adapters/LoggerAdapter.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Structural.Adapter.Log.Interfaces;
+using System;
+
+namespace DesignPattern.Structural.Adapter.Log
+{
+    /// <summary>
+    /// Adapter
+    /// </summary>
+    public class LoggerAdapter : ILog
+    {
+        private readonly IAnotherLog _anotherLog;
+
+        public LoggerAdapter(IAnotherLog anotherLog)
+        {
+            _anotherLog = anotherLog;
+        }
+        public void Log(string message)
+        {
+            _anotherLog.Info(message);
+        }
+
+        public void LogError(Exception exception)
+        {
+            _anotherLog.Error(exception);
+        }
+    }
+}
