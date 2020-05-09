f45e761e5bfcc8f05e9b12e29f63a89eb0905e51
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-08 22:10:30 -0300

    [ADD] Added TransactionService class

---
+++ DesignPattern/src/DesignPattern/Structural/Adapter/Log/Services/TransactionService.cs
@@ -0,0 +1,36 @@
+﻿using DesignPattern.Structural.Adapter.Log.Interfaces;
+using System;
+
+namespace DesignPattern.Structural.Adapter.Log.Services
+{
+    /// <summary>
+    /// Client 
+    /// </summary>
+    public class TransactionService
+    {
+        private readonly ILog _log;
+
+        public TransactionService(ILog log)
+        {
+            _log = log;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                _log.Log("Initializing Transaction....");
+
+                // Do any transaction here
+
+                _log.Log("Finishing Transaction...");
+            }
+            catch (Exception ex)
+            {
+
+                _log.LogError(ex);
+            }
+
+        }
+    }
+}
