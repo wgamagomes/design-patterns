d465216d7308d6ec6b8e8cb3b76ae1248afb6bac
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-13 10:37:24 -0300

    [ADD] Added ValidatorBuilder class

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/Validation/ValidatorBuilder.cs
@@ -0,0 +1,28 @@
+﻿using DesignPattern.Creational.Builder.Validation.Interfaces;
+using System.Collections.Generic;
+
+namespace DesignPattern.Creational.Builder.Validation
+{
+    public class ValidatorBuilder<TValidable>
+        where TValidable: IValidable
+    {
+        private readonly List<IValidator<TValidable>> _validators;
+
+        public ValidatorBuilder()
+        {
+            _validators = new List<IValidator<TValidable>>();
+        }
+
+        public static ValidatorBuilder<TValidable> Create()
+        {
+            return new ValidatorBuilder<TValidable>();
+        }
+
+        public ValidatorBuilder<TValidable> AddValidator(IValidator<TValidable> validator)
+        {
+            _validators.Add(validator);
+
+            return this;
+        }
+    }
+}
