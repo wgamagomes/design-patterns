8db5a8ec9938c3a76771fdcd763fad4985a1cbb0
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-13 10:35:02 -0300

    [ADD] Added IValidator interface

---
+++ DesignPattern/src/DesignPattern/Creational/Builder/Validation/Interfaces/IValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational.Builder.Validation.Interfaces
+{
+    public interface IValidator<TValidable>
+    {
+        void Validate(TValidable @object);
+    }
+}
