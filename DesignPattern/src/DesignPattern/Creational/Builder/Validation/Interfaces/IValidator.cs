a72af94bb787507b5bbcb498a7a7dcf15f6ce1a3
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-20 18:07:12 -0300

    [ALT]  Changed usisng

--- DesignPattern/src/DesignPattern/Creational/Builder/Validation/Interfaces/IValidator.cs
+++ DesignPattern/src/DesignPattern/Creational/Builder/Validation/Interfaces/IValidator.cs
@@ -1,8 +1,10 @@
-﻿namespace DesignPattern.Creational.Builder.Validation.Interfaces
+﻿using DesignPattern.Creational.Builder.Validation.Result;
+
+namespace DesignPattern.Creational.Builder.Validation.Interfaces
 {
     public interface IValidator<TValidable>
          where TValidable : IValidable
     {
-        void Validate(TValidable @object);
+        ValidationResult Validate(TValidable @object);
     }
 }
